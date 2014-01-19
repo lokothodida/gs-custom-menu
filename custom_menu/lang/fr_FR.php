f4de40f1af1f655378e542aa9fa54f9850574b45
Emmanuel еΜanwʬĔbdƎv <emanwebdev@gmail.com>
2014-01-19 17:52:18 +0100

    Added fr_FR.php
    Human translation

---
+++ custom_menu/lang/fr_FR.php
@@ -0,0 +1,36 @@
+<?php
+
+// French language file for Custom Menu
+$i18n = array(
+  // plugin
+    'PLUGIN_NAME' => 'Custom Menu: Menu Personnalisé',
+    'PLUGIN_DESC' => 'Création de menus de navigation personnalisés',
+    'PLUGIN_SIDEBAR' => 'Personnaliser les menus',
+  
+  // admin
+    'NAME' => 'Nom',
+    'TITLE' => 'Titre du menu',
+    'URL' => 'URL / lien',
+    'SLUG' => 'Identifiant / Slug',
+    'TARGET' => 'Attribut "target"',
+    'MENU' => 'Menu',
+    'ITEM' => 'Ajouter un élément',
+    'ITEMS' => 'Ajouter des éléments',
+    'NODE' => 'Nœud',
+    'IMAGE' => 'Image: URL ou dans Fichiers mis en ligne',
+    'CLASS_CURRENTPATH' => 'Classe du "Chemin Courant"',
+    'CLASS_CURRENT' => 'Classe du menu "Courant"',
+    'CREATE' => 'Créer',
+    'ARE_YOU_SURE' => 'Etes-vous sûr de vouloir enregistrer ce menu?',
+    'MENU_CREATE_SUCCESS' => 'Le menu a bien été créé',
+    'MENU_CREATE_ERROR' => 'Erreur lors de la création du menu',
+    'MENU_UPDATE_SUCCESS' => 'Le menu %s a bien été mis à jour',
+    'MENU_CREATE_ERROR' => 'Erreur lors de la mise à jour du menu',
+    'ARE_YOU_SURE_DEL' => 'Etes-vous sûr de vouloir supprimer ce menu?',
+    'MENU_DEL_SUCCESS' => 'Le menu %s a été supprimé',
+    'MENU_DEL_ERROR' => 'Erreur lors de la suppression du menu',
+    'NO_MENUS' => 'Aucun menu pour l\'instant',
+    'BACK' => 'Annuler',
+);
+
+?>
