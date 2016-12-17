d61c91686f55eb12fe025d021005ff7588d5b40f
Lawrence Okoth-Odida <lawrence.okothodida@gmail.com>
2016-12-17 15:32:34 +0000

    Break up placeholder callback into smaller functions

--- custom_menu/php/placeholder.class.php
+++ custom_menu/php/placeholder.class.php
@@ -2,30 +2,39 @@
 
 class CustomMenuPlaceholder {
   static public function filter($content) {
-    $match = preg_match_all('/(<p>\s*)?\(%( )*'. CustomMenu::FILE . '(.*?)( )*%\)(\s*<\/p>)?/', $content, $matches);
+    $regex = '/\(% ' . CustomMenu::FILE . '(.*?)%\)/i';
 
-    if (isset($matches[3])) {
-      foreach ($matches[3] as $key => $params) {
-        $params = explode(',', $params);
-        $params = str_replace(array('\'', '"'), '', $params);
-        $params = array_map('trim', $params);
+    return preg_replace_callback($regex, 'self::callback', $content);
+  }
 
-        // evaluate boolean parameters
-        foreach ($params as $k => $par) {
-          if (strtolower($par) === 'true')  $params[$k] = true;
-          if (strtolower($par) === 'false') $params[$k] = false;
-        }
-        if (empty($params[0])) $params[0] = 'default';
+  static protected function callback($matches) {
+    // Get the parameters
+    $params = self::parseParameters($matches[1]);
 
-        ob_start();
-          $menu = new CustomMenuDisplay($params[0]);
-          $output = '<ul class="'. CustomMenu::FILE . ' ' . $params[0] . '">' . ob_get_contents() . '</ul>';
-        ob_end_clean();
+    ob_start();
+      $menu = new CustomMenuDisplay($params[0]);
+      $output = '<ul class="'. CustomMenu::FILE . ' ' . $params[0] . '">' . ob_get_contents() . '</ul>';
+    ob_end_clean();
 
-        $content = str_replace($matches[0][$key], $output, $content);
-      }
+    return $output;
+  }
+
+  static protected function parseParameters($string) {
+    $params = explode(',', $string);
+    $params = str_replace(array('\'', '"'), '', $params);
+    $params = array_map('trim', $params);
+
+    // evaluate boolean parameters
+    foreach ($params as $k => $par) {
+      if (strtolower($par) === 'true')  $params[$k] = true;
+      if (strtolower($par) === 'false') $params[$k] = false;
     }
 
-    return $content;
+    // Set a default
+    if (empty($params[0])) {
+      $params[0] = 'default';
+    }
+
+    return $params;
   }
 }
